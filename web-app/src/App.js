b4ec105993da2c2acc0428333968db12292fb70f
P0CEE <antoine.fromentina@gmail.com>
2023-12-29 21:53:09 +0100

    Add automatic backups and redirect to newly created note

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,13 +1,24 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Toaster, toast } from 'sonner';
 
 import "./App.css";
 import Note from "./components/Note";
 
-
 function App() {
-  // déclarer l'état pour stocker les notes
-  const [notes, setNotes] = useState(null);
+  return (
+    <div>
+      <Toaster richColors position="top-center"/>
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+function Main() {
+  const [notes, setNotes] = React.useState(null);
+  const navigate = useNavigate();
 
   async function fetchNotes() {
     const response = await fetch("/notes?_sort=id&_order=desc");
@@ -16,20 +27,31 @@ function App() {
   }
 
   async function createNote() {
-    await fetch("/notes", {
+    const response = await fetch("/notes", {
       method: "POST",
       body: JSON.stringify({ title: "Nouvelle note", content: "" }),
       headers: { "Content-type": "application/json" },
     });
-    fetchNotes();
+
+    if (response.ok) {
+      const newNote = await response.json();
+      fetchNotes();
+
+      toast.success("Nouvelle note créée avec succès");
+
+      // Rediriger vers la note nouvellement créée
+      navigate(`/notes/${newNote.id}`);
+    } else {
+      toast.error("Erreur lors de la création de la note");
+    }
   }
 
-  useEffect(function () {
+  React.useEffect(() => {
     fetchNotes();
   }, []);
 
   return (
-    <BrowserRouter>
+    <div>
       <aside className="Side">
         <div>
           <button className="Button Button-create-note" onClick={createNote}>
@@ -50,13 +72,16 @@ function App() {
       </aside>
       <main className="Main">
         <Routes>
-          <Route path="/" elements="Sélectionner une note" />
-          <Route path="/notes/:id" element={<Note />} />
+          <Route path="/" element="Sélectionner une note" />
+          <Route
+            path="/notes/:id"
+            element={<Note onSaveSuccess={fetchNotes} />}
+          />
 
-          <Route path="*" element={<Navigate to="/" />} />
+          {/* Ajoutez d'autres routes au besoin */}
         </Routes>
       </main>
-    </BrowserRouter>
+    </div>
   );
 }
 
--- web-app/src/components/Note.js
+++ web-app/src/components/Note.js
@@ -1,21 +1,71 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-
+import { useEffect, useState } from "react";
 import "./Note.css";
 
-function Note() {
+function Note({ onSaveSuccess }) {
   const { id } = useParams();
+  const [note, setNote] = useState(null);
+  const [unsavedChanges, setUnsavedChanges] = useState(false);
 
+  useEffect(() => {
+    const fetchNote = async () => {
+      const response = await fetch(`/notes/${id}`);
+      const data = await response.json();
+      setNote(data);
+    };
+
+    fetchNote();
+  }, [id]);
+
+  useEffect(() => {
+    if (unsavedChanges) {
+      const saveNoteAutomatically = async () => {
+        if (note) {
+          await fetch(`/notes/${id}`, {
+            method: "PUT",
+            body: JSON.stringify(note),
+            headers: { "Content-Type": "application/json" },
+          });
+          onSaveSuccess();
+        }
+      };
+
+      const saveNoteTimeout = setTimeout(() => {
+        saveNoteAutomatically();
+        setUnsavedChanges(false);
+      }, 1000); // Ajustez la durée du délai selon vos besoins
+
+      return () => clearTimeout(saveNoteTimeout);
+    }
+  }, [unsavedChanges, note, id, onSaveSuccess]);
+
+  const handleTitleChange = (event) => {
+    setNote({ ...note, title: event.target.value });
+    setUnsavedChanges(true);
+  };
+
+  const handleContentChange = (event) => {
+    setNote({ ...note, content: event.target.value });
+    setUnsavedChanges(true);
+  };
+
+  if (!note) {
+    return "Chargement…";
+  }
 
   return (
     <form className="Form">
-      <input className="Note-editable Note-title" type="text" value={note.title} />
-
-      <textarea className="Note-editable Note-content" value={note.content} />
-
-      <div className="Note-actions">
-        <button className="Button">Enregistrer</button>
-      </div>
+      <input
+        className="Note-editable Note-title"
+        type="text"
+        value={note.title}
+        onChange={handleTitleChange}
+      />
+      <textarea
+        className="Note-editable Note-content"
+        value={note.content}
+        onChange={handleContentChange}
+      />
     </form>
   );
 }
